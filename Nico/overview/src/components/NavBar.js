7597b0af044b9b9221936d4a04be73cf28ba2884
jihyunglee <jhlee7125@gmail.com>
2023-05-02 19:33:55 +0900

    Apply CSS Module as two different ways

--- Nico/overview/src/components/NavBar.js
+++ Nico/overview/src/components/NavBar.js
@@ -5,11 +5,14 @@ import styles from './NavBar.module.css';
 export default function NavBar() {
   const router = useRouter();
   return (
-    <nav className={styles.nav}>
-      <Link href='/' className='Home' style={{ color: router.pathname === '/' ? 'red' : 'blue' }}>
+    <nav>
+      <Link href='/' className={`${styles.link} ${router.pathname === '/' ? styles.active : ''}`}>
         Home
       </Link>
-      <Link href='/about' style={{ color: router.pathname === '/about' ? 'red' : 'blue' }}>
+      <Link
+        href='/about'
+        className={[styles.link, router.pathname === '/about' ? styles.active : ''].join(' ')}
+      >
         About
       </Link>
     </nav>
