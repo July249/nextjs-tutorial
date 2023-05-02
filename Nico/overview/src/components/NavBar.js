d51dd045a47cc0c3f4cf2f62e4fd96500796a4eb
jihyunglee <jhlee7125@gmail.com>
2023-05-02 19:26:41 +0900

    CSS Modules

--- Nico/overview/src/components/NavBar.js
+++ Nico/overview/src/components/NavBar.js
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import styles from './NavBar.module.css';
 
 export default function NavBar() {
   const router = useRouter();
-  console.log(router);
   return (
-    <nav>
+    <nav className={styles.nav}>
       <Link href='/' className='Home' style={{ color: router.pathname === '/' ? 'red' : 'blue' }}>
         Home
       </Link>
