8da77060d9bef580899ece0607a92306238b8625
jihyunglee <jhlee7125@gmail.com>
2023-05-05 00:29:09 +0900

    Image 사이즈 최적화, lazy loading, layout shift 방지

---
+++ CA/fresh/app/list/page.js
@@ -0,0 +1,30 @@
+import Image from 'next/image'; // Image 최적화 => image를 무조건 import 해서 넣어줘야함
+import TomatoImg from '/public/food0.png';
+import PastaImg from '/public/food1.png';
+import CoconutImg from '/public/food2.png';
+
+export default function List() {
+  let product = [
+    { name: '토마토', price: '$100', src: TomatoImg },
+    { name: '파스타', price: '$10', src: PastaImg },
+    { name: '코코넛', price: '$200', src: CoconutImg },
+  ];
+  return (
+    <main>
+      <h2 className='title'>상품목록</h2>
+      {product.map((item, index) => (
+        <div className='food' key={index}>
+          <Image className='food-img' src={item.src} alt={item.name} />
+          {/* 
+            src에 직접 url을 넣어주는 경우 next.config.js에서 주석으로 된 설정을 추가해야함
+            추가로 width와 height 값을 넣어줘야함
+            ex. <Image className='food-img' src='https://www.example.com/asdfqwerasdf' alt={item.name} width={200} height={200} />
+          */}
+          <h3>
+            {item.name} <span>{item.price}</span>
+          </h3>
+        </div>
+      ))}
+    </main>
+  );
+}
--- CA/fresh/next.config.js
+++ CA/fresh/next.config.js
@@ -3,6 +3,16 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  // images: {
+  //   remotePatterns: [
+  //     {
+  //       protocol: 'https',
+  //       hostname: 'cdn.example.com',
+  //       port: '',
+  //       path: '/:path*', // :path* is required
+  //     },
+  //   ],
+  // },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
