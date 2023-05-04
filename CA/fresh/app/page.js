7885c2adcbb5ca6afe84792108dafa0110412ca3
jihyunglee <jhlee7125@gmail.com>
2023-05-05 00:38:51 +0900

    main 페이지

--- CA/fresh/app/page.js
+++ CA/fresh/app/page.js
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1 className='title'>애플후레시</h1>
+      <p className='title-sub'>Welcome!</p>
+    </div>
+  );
 }
