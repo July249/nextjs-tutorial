1235403bc84e66c2d965140bb2c9aff07a08c5cd
jihyunglee <jhlee7125@gmail.com>
2023-05-06 04:55:03 +0900

    try-catch 문 사용

--- CA/my-board/pages/api/post/newPost.js
+++ CA/my-board/pages/api/post/newPost.js
@@ -1,21 +1,24 @@
 import { connectDB } from '@/util/db';
 
-export default async function uploadPost(req, res) {
+export default async function newPost(req, res) {
   const client = await connectDB;
   const db = client.db('my-board');
 
   if (req.method === 'POST') {
     if (!req.body.title || !req.body.content) {
       return res.status(400).json('Bad Request');
-    } else {
+    }
+    try {
       const post = await db.collection('post').insertOne({
-        title: data.title,
-        content: data.content,
+        title: req.body.title,
+        content: req.body.content,
         date: new Date(),
       });
 
       // 응답과 동시에 페이지 이동
-      return res.status(302).redirect('/list');
+      res.redirect(302, '/list');
+    } catch (error) {
+      console.error(error);
     }
   }
 }
