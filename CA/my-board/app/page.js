63293a99c14e49dd411875b3c1039cad16442cee
jihyunglee <jhlee7125@gmail.com>
2023-05-05 16:43:14 +0900

    chore: remove useless code

--- CA/my-board/app/page.js
+++ CA/my-board/app/page.js
@@ -5,7 +5,6 @@ export default async function Home() {
   const db = client.db('my-board');
   // post 컬렉션 조회
   const result = await db.collection('post').find().toArray();
-  console.log(result);
   return (
     <div>
       <h1>{result[0].title}</h1>
