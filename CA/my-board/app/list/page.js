da912c1a7b4bfd4fee1e5501e0aa0ffc9f11970b
jihyunglee <jhlee7125@gmail.com>
2023-05-05 17:07:27 +0900

    connect link between list and detail

--- CA/my-board/app/list/page.js
+++ CA/my-board/app/list/page.js
@@ -1,16 +1,20 @@
 import { connectDB } from '@/util/db';
+import Link from 'next/link';
 
 export default async function List() {
   const client = await connectDB;
   const db = client.db('my-board');
   const result = await db.collection('post').find().toArray();
+  console.log(result);
   return (
     <div className='list-bg'>
       {result.map((item, index) => (
-        <div className='list-item' key={index}>
-          <h4>{item.title}</h4>
-          <p>1월 1일</p>
-        </div>
+        <Link href={`/detail/${item._id}`} className='list-link' key={index}>
+          <div className='list-item'>
+            <h4>{item.title}</h4>
+            <p>1월 1일</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
