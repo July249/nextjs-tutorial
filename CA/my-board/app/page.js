ea627573214d71f34e92ca2ad5309ba95212ab04
jihyunglee <jhlee7125@gmail.com>
2023-05-05 16:24:09 +0900

    connect MongoDB

--- CA/my-board/app/page.js
+++ CA/my-board/app/page.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>메인</div>;
+import { connectDB } from '@/util/db';
+
+export default async function Home() {
+  const client = await connectDB;
+  const db = client.db('my-board');
+  // post 컬렉션 조회
+  const result = await db.collection('post').find().toArray();
+  console.log(result);
+  return (
+    <div>
+      <h1>{result[0].title}</h1>
+      <p>{result[0].content}</p>
+    </div>
+  );
 }
