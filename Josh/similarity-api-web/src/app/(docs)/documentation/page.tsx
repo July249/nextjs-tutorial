e2fef8798c9288e174b929f6848e8b87c47c2149
jihyunglee <jhlee7125@gmail.com>
2023-05-06 20:23:09 +0900

    design: styling documentation layout and page

--- Josh/similarity-api-web/src/app/(docs)/documentation/page.tsx
+++ Josh/similarity-api-web/src/app/(docs)/documentation/page.tsx
@@ -1,9 +1,27 @@
+import LargeHeading from '@/components/ui/LargeHeading';
+import Paragraph from '@/components/ui/Paragraph';
 import { FC } from 'react';
+import 'simplebar-react/dist/simplebar.min.css';
 
-interface pageProps {}
+import DocumentationTabs from '@/components/DocumentationTabs';
+import type { Metadata } from 'next';
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>;
+export const metadata: Metadata = {
+  title: 'Similarity API | Documentation',
+  description: 'Free & open-source text similarity API',
+};
+
+const page: FC = () => {
+  return (
+    <div className='container max-w-7xl mx-auto mt-12'>
+      <div className='flex flex-col items-center gap-6'>
+        <LargeHeading>Making a request</LargeHeading>
+        <Paragraph>api/v1/similarity</Paragraph>
+
+        <DocumentationTabs />
+      </div>
+    </div>
+  );
 };
 
 export default page;
