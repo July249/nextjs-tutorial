6f46e62002960691888ff6315349016822f72fba
jihyunglee <jhlee7125@gmail.com>
2023-05-06 19:52:18 +0900

    feat: metadata title, description

--- Josh/similarity-api-web/src/app/page.tsx
+++ Josh/similarity-api-web/src/app/page.tsx
@@ -1,6 +1,10 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import Paragraph from '@/components/ui/Paragraph';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Home',
+  description: 'Free & open-source text similarity API',
+};
 
 const inter = Inter({ subsets: ['latin'] });
 
