a59525a35e9087ea512f128aef8a2e76f12dcb6e
jihyunglee <jhlee7125@gmail.com>
2023-05-26 23:45:08 +0900

    02 Error Boundaries

--- Dave/next02/app/about/page.tsx
+++ Dave/next02/app/about/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link';
 
 export default function About() {
+  // To see the error boundary in action
+  throw new Error('Error in About');
   return (
     <>
       <h1>About</h1>
